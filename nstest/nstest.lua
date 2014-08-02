b511f79b3656afa49dc68e424e075a4d711163fc
Wuerfel_21 <nunuu@gmx.net>
2014-08-02 18:33:46 +0200

    added more stuff

--- nstest/nstest.lua
+++ nstest/nstest.lua
@@ -4,9 +4,11 @@ local shell = require("shell")
 local args = shell.parse(...)
 f = io.open(args[1])
 while true do
+::concurr::
 local l = f:read("*line")
 if l == nil then break end
 local w = notestream.do_command(l) / 1000
+if w == 0 then goto concurr end
 os.sleep(w)
 end
 f:close()
