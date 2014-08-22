3662b5df6b243638372fd6eead8858125eb74a84
Wuerfel_21 <nunuu@gmx.net>
2014-08-22 19:14:50 +0200

    made loader less annoyng

--- NoteAL/noteinit.lua
+++ NoteAL/noteinit.lua
@@ -2,9 +2,7 @@
 --Find license in license.txt
 local shell = require("shell")
 local confutil = require("noteal.confutil")
-print("\nNoteAL loader by Wuerfel_21")
-print("NoteAL "..confutil.version.." mounted in /usr/lib/noteal!")
-print("If you want to use NoteAL run \"notehelp\" to get more informtion")
+print("NoteAL V"..confutil.version.." loaded! Run notehelp for more information.")
 shell.setAlias("noteconf","/usr/lib/noteal/noteconf")
 shell.setAlias("eventlog","cat /usr/tmp/event.log")
 shell.setAlias("notehelp","edit /usr/lib/noteal/read.me -r")
